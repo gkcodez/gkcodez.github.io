8085b241fbdb5072b95646d440e621516bb42a29
gkcodez <gopalakrishnan.work@gmail.com>
2024-11-07 17:45:12 +0530

    Theme switcher implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Roboto, Poppins } from 'next/font/google'
 
 import './globals.css'
+import { Providers } from './providers'
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300', '500', '700', '900'],  variable: '--font-roboto'})
 const poppins = Poppins({ subsets: ['latin'], weight: ['100', '300', '500', '700', '900'], variable: '--font-poppins'})
@@ -17,8 +18,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={roboto.className}>
+          <Providers>
+            {children}
+          </Providers>
+        </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,26 @@
 import { IoLogoLinkedin, IoLogoGithub, IoMdMail } from "react-icons/io";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-start p-20'>
-      <h1 className='text-5xl font-bold p-2 font-poppins'>Gopalakrishnan</h1>
+      <h1 className='text-amber-600 text-5xl font-bold p-2 font-poppins'>Gopalakrishnan</h1>
       <h3 className='text-lg text-gray-500'>Web Developer & Bug Bounty Hunter</h3>
       <div className="social flex justify-center align-middle mt-3">
-        <a href="https://www.github.com/gkcodez" target="_blank"><IoLogoGithub className="text-6xl p-2 mr-1"/></a>
-        <a href="https://www.linkedin.com/in/gkcodez" target="_blank"><IoLogoLinkedin className="text-sky-500 text-6xl p-2 mr-1"/></a>
-        <a href="mailto:gopalakrishnan.work@gmail.com" target="_blank"><IoMdMail  className="text-6xl p-2 mr-1"/></a>
+        <a href="https://www.github.com/gkcodez" target="_blank"><IoLogoGithub className="text-6xl p-2"/></a>
+        <a href="https://www.linkedin.com/in/gkcodez" target="_blank"><IoLogoLinkedin className="text-6xl p-2"/></a>
+        <a href="mailto:gopalakrishnan.work@gmail.com" target="_blank"><IoMdMail  className=" text-6xl p-2"/></a>
       </div>
-      <nav className="mt-5">
-        <button className="nav-button p-2 mr-1">About</button>
-        <button className="nav-button p-2 mr-1">Projects</button>
-        <button className="nav-button p-2 mr-1">Contact</button>
+      <nav className="mt-5 flex items-center justify-center">
+        <button type="button" className="border-2 border-gray-700 rounded-lg text-xl p-2 mr-2">About</button>
+        <button type="button" className="border-2 border-gray-700 rounded-lg text-xl p-2 mr-2">Projects</button>
+        <button type="button" className="border-2 border-gray-700 rounded-lg text-xl p-2 mr-2">Contact</button>
+        <ThemeSwitcher/>
+        
       </nav>
 
-      <div id="about-section" className="text-lg mt-2">
-      <p className="mt-2">I'm <span className="bg-gray-600 p-3 rounded-md">&gt; Gopalakrishnan / GK</span>, Software engineer with a decade of experience in testing and developing enterprise applications.</p>
+      <div id="about-section" className="text-lg mt-3">
+      <p className="mt-2">I'm Gopalakrishnan / GK, Software engineer with a decade of experience in testing and developing enterprise applications.</p>
       <p className="mt-2"><span className="text-3xl">⚙️</span> I like automating everything around. Am I being smart? Nah! just too lazy to repeat things!</p>
       <p className="mt-2"><span className="text-3xl">🥰</span> Fell in love with hacking. Please don't ask me to hack facebook accounts though!</p>
       <p className="mt-2"><span className="text-3xl">😎</span> Passion for developing software. At times, I switch my developer mode ON!</p>
