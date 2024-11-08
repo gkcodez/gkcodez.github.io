cabfc3b45ec85f82f882e0487ce5f639ec40b767
gkcodez <gopalakrishnan.work@gmail.com>
2024-11-08 08:35:11 +0530

    Projects and tags added

--- app/(pages)/project/page.tsx
+++ app/(pages)/project/page.tsx
@@ -2,11 +2,38 @@ import ProjectCard from "@/app/components/ProjectCard"
 
 export default function Project() {
   return <div id="project-section" className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-4 max-w-7xl">
-    <ProjectCard title="Test title 1" description="Test description 1" thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg" codesrc="#" demosrc="#" />
-    <ProjectCard title="Test title 2" description="Test description 2" thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg" codesrc="#" />
-    <ProjectCard title="Test title 3" description="Test description 3" thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg" demosrc="#" />
-    <ProjectCard title="Test title 3" description="Test description 3" thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg" demosrc="#" />
-    <ProjectCard title="Test title 3" description="Test description 3" thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg" demosrc="#" />
-    <ProjectCard title="Test title 3" description="Test description 3" thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg" demosrc="#" />
+    <ProjectCard title="Employee Portal"
+      description="Full stack web application for managing employee data."
+      tags="React, Node.js, SQL server"
+      thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg"
+      codesrc="https://github.com/gkcodez/sern-employee-portal"
+    />
+    <ProjectCard title="Bug bounty reports"
+      description="Automation tool to fetch bug bounty reports from hackerone."
+      tags="Python"
+      thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg"
+      codesrc="https://github.com/gkcodez/bug-bounty-reports-hackerone"
+    />
+        <ProjectCard title="Portfolio"
+      description="Portfolio web application built with next.js and typescript."
+      tags="Next.js, Typescript, Tailwind CSS"
+      thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg"
+      codesrc="https://github.com/gkcodez/bug-bounty-reports-hackerone"
+      demosrc="https:gopalakrishnan.vercel.app"
+    />
+            <ProjectCard title="Autobot"
+      description="Web application with most used web elements for UI test automation practice."
+      tags="React.js, Tailwind CSS"
+      thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg"
+      codesrc="https://github.com/gkcodez/autobot"
+      demosrc="https:auto-bot-demo.vercel.app"
+    />
+            <ProjectCard title="Pomodoro"
+      description="Pomodoro application for productivity."
+      tags="Next.js, Tailwind CSS"
+      thumbnailsrc="https://cdn.pixabay.com/photo/2018/04/22/22/57/hacker-3342696_1280.jpg"
+      codesrc="https://github.com/gkcodez/next-pomodoro"
+      demosrc="https://pomodoro-byteappz.vercel.app/"
+    />
   </div>
 }
--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image"
 
 export default function ProjectCard(props: any) {
-    return (
-        <div className="project-card border-2 border-gray-600 p-5 rounded-lg">
+  let tags = null
+  if (props.tags) {
+    tags = props.tags.split(",")
+  }
+  return (
+    <div className="project-card flex flex-col h-full w-full border-2 border-gray-600 p-5 rounded-lg">
+      <div className="details grow">
         <Image
           src={props.thumbnailsrc}
           width={0}
@@ -12,11 +17,26 @@ export default function ProjectCard(props: any) {
           title={props.title}
           className="rounded-xl my-2 w-full h-auto"
         />
-        <h2 className="text-xl my-1">{props.title}</h2>
+        <h2 className="text-xl my-1 text-sky-600">{props.title}</h2>
         <p className="font-normal my-1">{props.description}</p>
-        <div className="buttons flex flex-wrap items-center justify-start gap-3 my-2">
-          { props.codesrc &&
-          <a href={props.codesrc} target="_blank" className="p-3 border-2 border-gray-500 text-gray-500 rounded-lg">View Code</a>
+        {
+          tags &&
+          <div className="flex flex-wrap gap-2 my-2">
+            {
+              tags.map((tag: any) => {
+                return (
+                  <p className="p-2 bg-gray-300 rounded-full text-sm">{tag}</p>
+                )
+              })
+            }
+          </div>
+        }
+      </div>
+      <div>
+        <hr />
+        <div className="buttons flex flex-wrap items-center justify-start gap-3 mt-3">
+          {props.codesrc &&
+            <a href={props.codesrc} target="_blank" className="p-3 border-2 border-gray-500 text-gray-500 rounded-lg">View Code</a>
           }
           {
             props.demosrc &&
@@ -24,5 +44,6 @@ export default function ProjectCard(props: any) {
           }
         </div>
       </div>
-    )
+    </div>
+  )
 }
